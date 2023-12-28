71d3e7fcfa5b0741872e5f65f4f8dd67dea2d6a8
Wasif Kamran <muwasifk@hotmail.com>
2023-12-27 19:17:50 -0500

    Add rudimentary solution

--- src/solve.py
+++ src/solve.py
@@ -21,36 +21,113 @@ import random
 
 
 class Teacher:
-    def __init__(self, schedule: list[list[str]]) -> None:
+    def __init__(self, name: str, schedule: list[list[str]]) -> None:
+        self.name = name
         self.schedule = schedule
-        self.schedule[0] = [
-            random.choice(["TAKEN", "SPARE"]),
-            random.choice(["TAKEN", "SPARE"]),
-            random.choice(["TAKEN", "SPARE"]),
-            random.choice(["TAKEN", "SPARE"]),
-        ]
+        self.dor: int = 0
+        self.potential: list[int] = []
+        if len(schedule[0]) == 0:
+            self.schedule[0] = [
+                random.choice(["TAKEN", "SPARE"]),
+                random.choice(["TAKEN", "SPARE"]),
+                random.choice(["TAKEN", "SPARE"]),
+                random.choice(["TAKEN", "SPARE"]),
+            ]
 
-        self.schedule[1] = [
-            random.choice(["TAKEN", "SPARE"]),
-            random.choice(["TAKEN", "SPARE"]),
-            random.choice(["TAKEN", "SPARE"]),
-            random.choice(["TAKEN", "SPARE"]),
-        ]
-        self.schedule[2] = [
-            self.schedule[0][1],
-            self.schedule[0][0],
-            self.schedule[0][3],
-            self.schedule[0][2],
-        ]
-        self.schedule[3] = [
-            self.schedule[1][1],
-            self.schedule[1][0],
-            self.schedule[1][3],
-            self.schedule[1][2],
-        ]
+            self.schedule[1] = [
+                random.choice(["TAKEN", "SPARE"]),
+                random.choice(["TAKEN", "SPARE"]),
+                random.choice(["TAKEN", "SPARE"]),
+                random.choice(["TAKEN", "SPARE"]),
+            ]
+            self.schedule[2] = [
+                self.schedule[0][1],
+                self.schedule[0][0],
+                self.schedule[0][3],
+                self.schedule[0][2],
+            ]
+            self.schedule[3] = [
+                self.schedule[1][1],
+                self.schedule[1][0],
+                self.schedule[1][3],
+                self.schedule[1][2],
+            ]
 
 
 teachers: list[Teacher] = []
-for i in range(72):
-    cur = Teacher([[], [], [], []])
+for i in range(75):
+    cur = Teacher("TEACH" + str(i), [[], [], [], []])
     teachers.append(cur)
+    if cur.schedule == ["TAKEN", "TAKEN", "TAKEN", "TAKEN"]:
+        print(cur.schedule)
+
+teachers.append(
+    Teacher(
+        "TEACH75",
+        [
+            ["TAKEN", "TAKEN", "TAKEN", "TAKEN"],
+            ["TAKEN", "TAKEN", "TAKEN", "TAKEN"],
+            ["TAKEN", "TAKEN", "TAKEN", "TAKEN"],
+            ["TAKEN", "TAKEN", "TAKEN", "TAKEN"],
+        ],
+    )
+)
+teachers.append(
+    Teacher(
+        "TEACH76",
+        [
+            ["SPARE", "TAKEN", "TAKEN", "TAKEN"],
+            ["TAKEN", "TAKEN", "TAKEN", "TAKEN"],
+            ["TAKEN", "SPARE", "TAKEN", "TAKEN"],
+            ["TAKEN", "TAKEN", "TAKEN", "TAKEN"],
+        ],
+    )
+)
+
+candidates: list[list[str]] = [[], [], [], [], [], [], [], [], [], [], [], []]
+
+for teacher in teachers:
+    for day in range(0, 4):
+        if teacher.schedule[day][1] == "SPARE" or teacher.schedule[day][2] == "SPARE":
+            candidates[day].append(teacher.name)
+
+            candidates[day + 4].append(teacher.name)
+
+            candidates[day + 8].append(teacher.name)
+
+            teacher.potential.append(day)
+            teacher.potential.append(day + 4)
+            teacher.potential.append(day + 8)
+
+dors: list[int] = []
+for i in range(len(teachers)):
+    dors.append(0)
+
+for teacher in teachers:
+    for cur_day in candidates:
+        if teacher.name in cur_day:
+            dors[int(teacher.name[5:])] += 1
+
+for teacher in teachers:
+    teacher.dor = dors[int(teacher.name[5:])]
+
+sorted_teachers = sorted(teachers, key=lambda x: x.dor)
+
+construction: list[list[str]] = [[], [], [], [], [], [], [], [], [], [], [], []]
+
+for teacher in sorted_teachers:
+    if teacher.dor == 0 or len(teacher.potential) == 0:
+        continue
+
+    choice = random.choice(teacher.potential)
+    while len(construction[choice]) == 6:
+        choice = random.choice(teacher.potential)
+
+    construction[choice].append(teacher.name)
+
+    if len(construction[choice]) == 6:
+        for alt_teacher in sorted_teachers:
+            if choice in alt_teacher.potential:
+                alt_teacher.potential.remove(choice)
+
+print(construction)
