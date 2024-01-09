5376000abb84e2981ee37cf3c01b304fc8e47b82
Wasif Kamran <muwasifk@hotmail.com>
2024-01-09 01:15:58 -0500

    Organized file structure

--- supervision/settings.py
+++ supervision/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "django.contrib.sites",
     "django.contrib.flatpages",
-    "markdownify.apps.MarkdownifyConfig",
 ]
 
 MARKDOWNIFY_WHITELIST_TAGS = [
