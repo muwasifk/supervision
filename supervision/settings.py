0b282821ea4ce277e06fd6c88ddbbb0901135916
Wasif Kamran <muwasifk@hotmail.com>
2024-02-04 23:29:05 -0500

    Create app and connect DB

--- supervision/settings.py
+++ supervision/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = "supervision.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': 'aws-0-us-west-1.pooler.supabase.com',
+        'NAME': 'postgres',
+        'USER': 'postgres.wumirxuozmjjednguidm',
+        'PORT': '5432',
+        'PASSWORD': 'CorrectHorseBatteryStaple>>1',
     }
 }
 
