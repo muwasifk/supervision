5fe6ba9e7a0c04fec9b62c0a6ad689ba68d08f01
Wasif Kamran <muwasifk@hotmail.com>
2024-05-20 16:02:38 -0400

    Comment calendar

--- core/models.py
+++ core/models.py
@@ -1,9 +1,18 @@
+"""
+models.py
+ICS4U SDP 
+Muhammad Wasif Kamran, Karan Chawla, Eric Sui 
+Classes to represent Django models / tables in database 
+"""
+# Django imports
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
 from django.contrib.auth.models import User
 
-
 class Schedule(models.Model):
+    """
+    Model to represent a schedule 
+    """
     schedule_id = models.CharField(max_length=255)
     name = models.CharField(max_length=255)
     first_name = models.CharField(max_length=255)
@@ -15,6 +24,9 @@ class Schedule(models.Model):
 
 
 class Teacher(models.Model):
+    """
+    Model to represent a teacher
+    """
     schedule_id = models.CharField(max_length=255)
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
@@ -24,5 +36,8 @@ class Teacher(models.Model):
 
 
 class ScheduleList(models.Model):
+    """
+    Model to represent a list of schedules 
+    """
     email = models.EmailField(max_length=255)
     schedules = ArrayField(models.CharField(max_length=255), size=0)
--- core/views/calendar.py
+++ core/views/calendar.py
@@ -1,21 +1,45 @@
+"""
+calendar.py
+ICS4U SDP 
+Muhammad Wasif Kamran, Karan Chawla, Eric Sui 
+CBV related to calendar generation and display 
+"""
+# Django imports
 from django.shortcuts import render
-
 from django.views.generic import TemplateView
+from django.http import FileResponse
+from core.models import Teacher, ScheduleList
+
+# Other Python imports for CSV and ICS handling as well as calendar manipulations 
+import random
 import datetime
 from calendar import monthrange
 from ics import Calendar, Event 
-from django.http import FileResponse
 import csv
-from core.models import Teacher, ScheduleList
-
-import random
 
 class ScheduleTeacher:
+    """
+    Class to represent a teacher in the schedule 
+    Attributes:
+        name: str
+        schedule: schedule (Django model)
+        dor: int (Degree of Restrictiveness)
+        potential: list[int]
+    """
+
     def __init__(self, name: str, schedule: list[list[str]]) -> None:
+        """
+        Initializes instance attributes
+        Args:
+            name: str
+            schedule: list[list[str]]
+        """
+        # Assign values to attributes 
         self.name = name
         self.schedule = schedule
         self.dor: int = 0
         self.potential: list[int] = []
+        # If no predefined schedule, set these ones (handles edge cases) 
         if len(schedule[0]) == 0:
             self.schedule[0] = [
                 random.choice(["TAKEN", "SPARE"]),
@@ -43,33 +67,51 @@ class ScheduleTeacher:
                 self.schedule[1][2],
             ]
 
-
-
+# global_iterator goes over all the days in the year
 global_iterator = 0
+# constructed calendar is represented by a 2D list 
 construction: list[list[str]] = [[], [], [], [], [], [], [], [], [], [], [], []]
+
 class CalendarView(TemplateView):
-    
+    """
+    CBV for the calendar page 
+    """    
+    # Define construction as global 
     global construction
+
     @staticmethod
     def construct_days(email):
+        """
+        Construct the teachers in a certain day period which will cycle over the year
+        Args:
+            email (str) 
+        Returns:
+            None 
+        """
+        # Read in list of teachers from database
         teachers: list[ScheduleTeacher] = []
         row = ScheduleList.objects.get(email=email)
         rows = Teacher.objects.all().filter(schedule_id=row.schedules[-1])
 
+        # Map a teacher to an index so that they can be accessed from the teachers list 
         teacher_index_map = {}
         for i in range(0, len(rows)):
+              # Parse schedule 
               schedule_string = rows[i].schedule
               schedule_list = schedule_string.split("/")
-              print(schedule_list)
+              # Split into the days 
               day1 = [schedule_list[0], schedule_list[1], schedule_list[2], schedule_list[3]]
               day2 = [schedule_list[4], schedule_list[5], schedule_list[6], schedule_list[7]] 
               day3 = [schedule_list[1], schedule_list[0], schedule_list[3], schedule_list[2]]
               day4 = [schedule_list[5], schedule_list[4], schedule_list[7], schedule_list[6]]
+              # Instantiate object and append to the list (also save the index at which the teacher is in the list)
               cur = ScheduleTeacher(rows[i].first_name + rows[i].last_name, [day1, day2, day3, day4])
               teachers.append(cur)
               teacher_index_map[rows[i].first_name + rows[i].last_name] = i
 
+        # Candidates for every day in a 12 day cycle 
         candidates: list[list[str]] = [[], [], [], [], [], [], [], [], [], [], [], []]
+        # For every teacher, if they have a spare before or after lunch, they are a possible candidate on that day 
         for teacher in teachers:
                 for day in range(0, 4):
                         if teacher.schedule[day][1] == "SPARE" or teacher.schedule[day][2] == "SPARE":
@@ -83,6 +125,7 @@ class CalendarView(TemplateView):
                                 teacher.potential.append(day + 4)
                                 teacher.potential.append(day + 8)
 
+        # Create a list of degrees of restrictiveness which says how restricted a teacher is (the less  restricted, the higher the dor) 
         dors: list[int] = []
         for i in range(len(teachers)):
                 dors.append(0)
@@ -90,16 +133,18 @@ class CalendarView(TemplateView):
                 for cur_day in candidates:
                         if teacher.name in cur_day:
                                 dors[teacher_index_map[teacher.name]] += 1
+
+        # Sort the teachers by ascending dor (most to least restricted) 
         for teacher in teachers:
                 teacher.dor = dors[teacher_index_map[teacher.name]]
         sorted_teachers = sorted(teachers, key=lambda x: x.dor)
 
         
-
+        # Fill in the teachers that are the most restricted and keep going until all teachers or spots are filled in
         for teacher in sorted_teachers:
                 if teacher.dor == 0 or len(teacher.potential) == 0:
                         continue
-
+                # Randomly fill in slots till they make a valid schedule
                 choice = random.choice(teacher.potential)
                 while len(construction[choice]) == 6:
                         choice = random.choice(teacher.potential)
@@ -110,13 +155,21 @@ class CalendarView(TemplateView):
                         for alt_teacher in sorted_teachers:
                                 if choice in alt_teacher.potential:
                                         alt_teacher.potential.remove(choice)
-    
+    # Global var to store where the teachers are going to be placed for the calendar
     global data
     data = [('Date', 'Name')]       
+
     @staticmethod
     def generate_calendar(day, month, year):
         """
         Generates a calendar template for a given month 
+        Args:
+            day: str 
+            month: str
+            year: str 
+        Returns:
+            str, str
+        Note: code might seem complex, but its really just writing HTML code to be injected 
         """
         # Creating a template string for what should be displayed on the frontend
         calendar_string = '<tr class="text-left align-text-top">'
@@ -153,6 +206,7 @@ class CalendarView(TemplateView):
                 calendar_string += f' <td class="h-24 border align-text-top px-2 py-1">{iterator} <br><br> {current[0]} <br> {current[1]} <br> {current[2]} <br> {current[3]} <br> {current[4]} <br> {current[5]}</td>'
                 global_iterator += 1
 
+                # Add events to the ICS file 
                 e1 = Event(); e1.name=current[0]; e1.begin = datetime.date(year, month, iterator)
                 e2 = Event(); e2.name=current[0]; e2.begin = datetime.date(year, month, iterator)
                 e3 = Event(); e3.name=current[0]; e3.begin = datetime.date(year, month, iterator)
@@ -209,15 +263,27 @@ class CalendarView(TemplateView):
                     calendar_string += ' <td class="h-24 border bg-gray-50 px-2"></td>'
                 iterator += 1
             calendar_string += " </tr>"
+
         # Getting the appropiate month name 
-        
         calendar_name = f"{month_name[month]} {year}"
+
         return calendar_string, calendar_name
+    
     def post(self, request):
+        """
+        Handle POST request from the calendar 
+        Args:
+            request: dict 
+        Returns: 
+            None 
+        """
+        # If the POST request is related to the ICS file, write to the ICS file and download it 
         if 'ics' in request.POST:
             with open('my.ics', 'w') as f:
                         f.writelines(c.serialize_iter())   
             return FileResponse(open('my.ics', 'rb'), as_attachment=True, filename="supervision.ics")
+        
+        # If the POST request is related to the CSV file, write the CSV file and download it
         if 'csv' in request.POST:
             with open('my.csv', 'w', newline='') as f:
                 writer = csv.writer(f)
@@ -226,6 +292,14 @@ class CalendarView(TemplateView):
             return FileResponse(open('my.csv', 'rb'), as_attachment=True, filename="supervision.csv")
 
     def get(self, request):
+        """
+        Handle GET requests from the calendar
+        Args:
+            request: dict
+        Returns:
+            HTML render
+        """
+        # Define the current school year
         year = 2024
         month = 9
         day = 1
@@ -254,22 +328,13 @@ class CalendarView(TemplateView):
         calendar_string10, calendar_name10 = self.generate_calendar(
             day, month - 3, year + 1
         )
-
-        teachers_list = [
-            ["John", "Cafeteria"],
-            ["Rex", "Weight Room"],
-            ["Wasif", "Library"],
-            ["Karan", "Bleachers"],
-            ["Eric", "Rover"],
-        ]
-        test = '<p class="font-bold">happy</p> <p class="font-bold">happy</p>'  # (calendar_name[get_teachers(1, teachers_list)[0]])
+ 
         return render(
             # Returning the calendar strings for the frontend
             request,
             "calendar.html",
             {
                 "day": 1,
-                "test": test,
                 "calendar_string1": calendar_string1,
                 "calendar_name1": calendar_name1,
                 "calendar_string2": calendar_string2,
@@ -292,11 +357,3 @@ class CalendarView(TemplateView):
                 "calendar_name10": calendar_name10,
             },
         )
-    
-    @staticmethod
-    def calendar_name(name, role):
-        return name, "-", role
-
-    @staticmethod
-    def get_teachers(day, teachers):
-        return teachers[day * 5 : day * 5 + 5]
